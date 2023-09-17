a36e7fe582e7105a4105ab37759c6dea05c01aa6
Rafael <rafaelab@users.noreply.github.com>
2023-09-17 20:45:12 +0200

    initial commit

---
+++ examples/example2.cpp
@@ -0,0 +1,28 @@
+#include "particleDB.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace particleDB;
+
+
+int main() {
+	
+	// read database for selected particles
+	std::vector<int> pIds = {13, 15, 211, 111};
+	Database* db = new Database(pIds);
+
+
+	// test retrieving particle properties
+	Particle p1 = db->particleFromId(211);
+	std::cout << p1 << std::endl;
+
+	try { // we expect an exception
+		Particle p2 = db->particleFromId(11);
+	} catch (...) {
+		std::cout << "There is not particle with id 11 in the database. An error would be thrown." << std::endl;
+	}
+
+
+	return 0;
+}
